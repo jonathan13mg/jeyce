a2c5e31dcc1090fee35ce6395f42c01300cc24e9
jonathan13mg <nathan13mg@gmail.com>
2026-08-20 11:06:54 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,32 +1,49 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-  updateDoc,
-  deleteDoc
+    initializeFirestore,
+    persistentLocalCache,
+    persistentSingleTabManager,
+    collection,
+    getDocs,
+    getDoc,
+    addDoc,
+    doc,
+    updateDoc,
+    deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAgMEodYhlPOkzmHBg9bDMX-Pi28WyqsJM",
-  authDomain: "jeyce-b4a6e.firebaseapp.com",
-  projectId: "jeyce-b4a6e",
-  storageBucket: "jeyce-b4a6e.firebasestorage.app",
-  messagingSenderId: "488863970055",
-  appId: "1:488863970055:web:ae1054bb11ed7e11264ba2"
+    apiKey: "AIzaSyAgMEodYhlPOkzmHBg9bDMX-Pi28WyqsJM",
+    authDomain: "jeyce-b4a6e.firebaseapp.com",
+    projectId: "jeyce-b4a6e",
+    storageBucket: "jeyce-b4a6e.firebasestorage.app",
+    messagingSenderId: "488863970055",
+    appId: "1:488863970055:web:ae1054bb11ed7e11264ba2"
 };
 
+
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+
+// Firestore con caché local
+const db = initializeFirestore(app, {
+
+    localCache: persistentLocalCache({
+        tabManager: persistentSingleTabManager()
+    })
+
+});
+
 
 export {
-  db,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-  updateDoc,
-  deleteDoc
+    db,
+    collection,
+    getDocs,
+    getDoc,
+    addDoc,
+    doc,
+    updateDoc,
+    deleteDoc
 };
